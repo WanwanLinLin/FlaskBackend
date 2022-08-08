6910091ad4eeee2d5c0c6359932725da4b8c1ebd
WanwanLinLin <2283816735@qq.com>
2022-08-08 18:36:13 +0800

    第六次提交

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -45,7 +45,7 @@ def get_base_category_list():
                 l_ = ThCategoryListModel.query.filter(ThCategoryListModel.category_par == y.id).all()
                 # print(y.id)
                 # print(y)
-                print(l_)
+                # print(l_)
                 if l_:
                     for z in l_:
 
@@ -76,47 +76,12 @@ def list_():
         return e.json()
     else:
         X = request.get_json()
-        print(X)
+        print("request.get_json()的值是：", X)
         # print("这里执行了——2")
         trademark_list = []
         attrs_list = []
         goods_list = []
 
-        # 第一种情况：前端不传参数
-        if not X:
-            all_info = list(Goods_se.find({}, {"_id": 0}))
-            attrs_info = list(Goods_se_attrs.find({}, {"_id": 0, "connect_name": 0}))
-            # print(attrs_info, "\n")
-            for x in all_info:
-                trademark_list.append({"tmId": x["tmId"], "tmName": x["tmName"]})
-            # 获取不重复的品牌数据
-            trademark_list = [dict(t) for t in set([tuple(d.items()) for d in trademark_list])]
-
-            for x_ in all_info:
-                attrs_list_ = list(Goods_se_attrs.find({"connect_name": x_["tmName"]}, {"_id": 0, "connect_name": 0}))
-                x_["attrs"] = attrs_list_
-                goods_list.append(x_)
-            # print(goods_list, "\n")
-
-            for y in attrs_info:
-                attrs_list.append(y)
-
-            return jsonify({
-                "code": 200,
-                "message": "成功",
-                "data": {
-                    "trademarkList": trademark_list,
-                    "attrsList": attrs_list,
-                    "goodsList": goods_list,
-                    "total": 11,
-                    "pageSize": 10,
-                    "pageNo": 1,
-                    "totalPages": 4
-                },
-                "ok": True
-            })
-
-        # 第二种情况，前端传递来了参数
         category1_id = request.json.get("category1Id")
         category2_id = request.json.get("category2Id")
         category3_id = request.json.get("category3Id")
@@ -127,16 +92,64 @@ def list_():
         order = request.json.get("order")
         page_no = request.json.get("pageNo")
         page_size = request.json.get("pageSize")
-        print("keyword: ", keyword)
+        # print("props的唯一唯一值是: ", props)
 
+        # 第一种情况：设置默认页面
+        if not category1_id and not category2_id and not category3_id and not category3_id and \
+                not category_name and not keyword and not \
+                props and not trademark:
+            all_info = list(Goods_se.find({"tmId": 1, "tmName": "桂林卤粉"}, {"_id": 0}))
+            attrs_info = list(Goods_se_attrs.find({"connect_name": "桂林卤粉"}, {"_id": 0, "connect_name": 0}))
+            # print(attrs_info, "\n")
+            for x in all_info:
+                trademark_list.append({"tmId": x["tmId"], "tmName": x["tmName"]})
+            # 获取不重复的品牌数据
+            trademark_list = [dict(t) for t in set([tuple(d.items()) for d in trademark_list])]
+
+            for x_ in all_info:
+                goods_list.append(x_)
+            # print(goods_list, "\n")
+
+            for y in attrs_info:
+                attrs_list.append(y)
+
+            # 首页默认分页效果
+            limit_start = (page_no - 1) * page_size
+            page_total = int(math.ceil(len(goods_list) / page_size))
+            goods_list = goods_list[limit_start:page_no * page_size]
+            return jsonify({
+                "code": 200,
+                "message": "成功",
+                "data": {
+                    "trademarkList": trademark_list,
+                    "attrsList": attrs_list,
+                    "goodsList": goods_list,
+                    "total": len(goods_list),
+                    "pageSize": page_size,
+                    "pageNo": page_no,
+                    "totalPages": page_total
+                },
+                "ok": True
+            })
+
+        # 第二种情况，前端传递来了参数（默认参数除外）
         # 先实现简单的分页效果
         # 该变量用于表示跳过前面多少条
         limit_start = (page_no - 1) * page_size
 
-        if category1_id:
+        # 处理 trademark 的值
+        if trademark:
+            trademark = trademark.split(":")
+            all_info_2 = list(Goods_se.find({"tmId": int(trademark[0]),
+                                             "tmName": trademark[1]},
+                                            {"_id": 0}))
+
+        # 处理由三级类目列表进行搜索的结果
+        elif category1_id:
             all_info_2 = list(Goods_se.find({"category1Id": category1_id,
                                              "category1Name": category_name},
                                             {"_id": 0}))
+
         elif category2_id:
             all_info_2 = list(Goods_se.find({"category2Id": category2_id,
                                              "category2Name": category_name},
@@ -145,7 +158,7 @@ def list_():
             all_info_2 = list(Goods_se.find({"category3Id": category3_id,
                                              "category3Name": category_name},
                                             {"_id": 0}))
-        print(all_info_2, "\n")
+        # print(all_info_2, "\n")
 
         if all_info_2 and keyword:
             # 这是对应同时使用三级类目导航和keyword导航的情况的情况
@@ -157,13 +170,9 @@ def list_():
             for x_ in all_info_2:
                 # if x_["tmName"] == keyword:
                 if keyword in x_["tmName"]:
-                    attrs_list_ = list(Goods_se_attrs.find({"connect_name": x_["tmName"]},
-                                                           {"_id": 0, "connect_name": 0}))
-                    x_["attrs"] = attrs_list_
+                    attrs_list = list(Goods_se_attrs.find({"connect_name": x_["tmName"]},
+                                                          {"_id": 0, "connect_name": 0}))
                     goods_list.append(x_)
-
-            if goods_list:
-                attrs_list += goods_list[0]["attrs"]
 
         elif all_info_2:
             # 这是对应只有三级类目导航的情况
@@ -171,13 +180,9 @@ def list_():
                 trademark_list.append({"tmId": x["tmId"], "tmName": x["tmName"]})
 
             for x_ in all_info_2:
-                attrs_list_ = list(Goods_se_attrs.find({"connect_name": x_["tmName"]},
-                                                       {"_id": 0, "connect_name": 0}))
-                x_["attrs"] = attrs_list_
+                attrs_list = list(Goods_se_attrs.find({"connect_name": x_["tmName"]},
+                                                      {"_id": 0, "connect_name": 0}))
                 goods_list.append(x_)
-
-            if goods_list:
-                attrs_list += goods_list[0]["attrs"]
 
         elif keyword:
             # 这是对应只有 keyword 的情况
@@ -190,13 +195,9 @@ def list_():
 
             for x_ in all_info_3:
                 if keyword in x_["tmName"]:
-                    attrs_list_ = list(Goods_se_attrs.find({"connect_name": x_["tmName"]},
-                                                       {"_id": 0, "connect_name": 0}))
-                    x_["attrs"] = attrs_list_
+                    attrs_list = list(Goods_se_attrs.find({"connect_name": x_["tmName"]},
+                                                          {"_id": 0, "connect_name": 0}))
                     goods_list.append(x_)
-            # .sort("id", 1).limit(page_size).skip(limit_start)
-            if goods_list:
-                attrs_list += goods_list[0]["attrs"]
 
         else:
             all_info_4 = list(Goods_se.find({}, {"_id": 0}))
@@ -208,17 +209,24 @@ def list_():
             trademark_list = [dict(t) for t in set([tuple(d.items()) for d in trademark_list])]
 
             for x_ in all_info_4:
-                attrs_list_ = list(Goods_se_attrs.find({"connect_name": x_["tmName"]}, {"_id": 0, "connect_name": 0}))
-                x_["attrs"] = attrs_list_
+                attrs_list = list(Goods_se_attrs.find({"connect_name": x_["tmName"]}, {"_id": 0, "connect_name": 0}))
                 goods_list.append(x_)
-            # print(goods_list, "\n")
-
-            for y in attrs_info:
-                attrs_list.append(y)
-
+        print(goods_list, "\n")
 
         # 采用切片方式方便分页
-        goods_list = goods_list[limit_start:page_no*page_size]
+        goods_list = goods_list[limit_start:page_no * page_size]
+        # print("goods_list的值是：", goods_list)
+
+        # 处理props参数
+        if props:
+            props_list = []
+            for x in props:
+                for z in goods_list:
+                    if x in z["attrs"]:
+                        if z not in props_list:
+                            props_list.append(z)
+            goods_list = props_list
+
         # 获取不重复的品牌数据
         trademark_list = [dict(t) for t in set([tuple(d.items()) for d in trademark_list])]
 
